cba37c663805f567a8ba82466e8f5fcd76500653
Junya SUZUKI <jsuzuki@stm-pi>
2021-03-18 21:53:12 +0900

    debugging

--- stm_max_client.py
+++ stm_max_client.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 '''Stimulator MAX board client'''
 from ocs.matched_client import MatchedClient
-
+from time import sleep
 
 def main():
     '''Stimulator MAX board client'''
     max_client = MatchedClient('stmmax1')
-    status, message, session = max_client.get_values(params={'channel': 0})
+    status, message, session = max_client.get_values(channel=0)
     print(status, message)
-    print(session.data)
+    print(session['data'])
 
+    sleep(1)
+
+    max_client.set_values(channel=0, tc_type=3)
+
+    sleep(1)
+
+    status, message, session = max_client.get_values(channel=0)
+    print(status, message)
+    print(session['data'])
+    
 
 if __name__ == '__main__':
     main()
